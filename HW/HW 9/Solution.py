e11eec851b68841a8eb139370a935cf7866de96a
chrisoh02 <144830381+chrisoh02@users.noreply.github.com>
2024-03-04 14:26:06 -0800

    Add files via upload

---
+++ HW/HW 9/Solution.py
@@ -0,0 +1,24 @@
+# Christopher Oh
+# SJSU CS 146
+# Homework 9
+
+import TreeNode
+
+
+class Solution:
+    def __init__(self):
+        print("Homework 9")
+
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root is None:
+            return None
+
+        if p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        elif p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        else:
+            return root
+
+
+
