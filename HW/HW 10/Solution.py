1b7b4c1729d2bd7cfd2af5e12f3266955fd2935f
chrisoh02 <144830381+chrisoh02@users.noreply.github.com>
2024-03-13 14:43:08 -0700

    Add files via upload

---
+++ HW/HW 10/Solution.py
@@ -0,0 +1,29 @@
+# Christopher Oh
+# SJSU CS 146
+# HW 10 - Binary Search Tree Level Order Traversal
+from typing import Optional, List
+import TreeNode
+
+class Solution:
+    def __init__(self):
+        print("HW 10")
+
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        nodes_by_level = []
+
+        if root is not None:
+            self.helper(root, nodes_by_level, 0)
+
+        return nodes_by_level
+
+    def helper(self, node, nodes_by_level, level):
+        while len(nodes_by_level) - 1 < level:
+            nodes_by_level.append([])
+
+        nodes_by_level[level].append(node.val)
+
+        if node.left is not None:
+            self.helper(node.left, nodes_by_level, level + 1)
+
+        if node.right is not None:
+            self.helper(node.right, nodes_by_level, level + 1)
